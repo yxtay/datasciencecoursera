1e4ba4145c1b5d4ecb260db2d42891a0c8e6e2ea
yxtay <wyextay@gmail.com>
2014-12-28 13:09:07 +0800

    Rearranged codes

--- 10-data-science-capstone/shiny/server.R
+++ 10-data-science-capstone/shiny/server.R
@@ -1,52 +1,6 @@
 library(shiny)
-library(plyr)
 
-load("RData/unigramdf.RData")
-load("RData/bigramdf2.RData")
-load("RData/trigramdf2.RData")
-# load("RData/quadgramdf2.RData")
-
-cleanText <- function(textInput) {
-    # remove non-printable characters i.e. not [:alnum:], [:punct:] and [:space:]
-    textInput <- gsub("[^[:print:]]+", " ", textInput)
-    # merge words/characters separated by period, aprostrophe and hyphen
-    textInput <- gsub("([[:alpha:]])[.'-]([[:alpha:]])", "\\1\\2", textInput)
-    # merge digits separated by punctuation and space
-    textInput <- gsub("([[:digit:]])[[:punct:] ]([[:digit:]])", "\\1\\2", textInput)
-    # replace ampersand with "and"
-    textInput <- gsub("&+", " and ", textInput)
-    # remove punctuations
-    textInput <- gsub("[[:punct:]]+", " ", textInput)
-    # remove non-alphnumeric characters
-    textInput <- gsub("[^[:alnum:]]+", " ", textInput)
-    # remove excessive whitespaces
-    textInput <- gsub(" +", " ", textInput)
-    # strip whitespace at beginning/end of sentences
-    textInput <- gsub("^ | $", "", textInput)
-    # convert all characters to lower case
-    textInput <- tolower(textInput)
-    # convert all numbers to "<NUM>"
-    textInput <- gsub("[[:digit:]]+", "<NUM>", textInput)
-    
-    wordsOut <- unlist(strsplit(textInput, " "))
-    return(wordsOut)
-}
-
-getNWords <- function(wordsIn, n) paste(tail(wordsIn, n), collapse = " ")
-
-suggestWords <- function(wordsInput, npred, ngramdf) {
-    wordsdf <- subset(ngramdf, words == wordsInput, c(suggestion, prop))
-    wordsdf <- arrange(wordsdf, desc(prop))
-    return(head(wordsdf, npred))
-}
-
-chooseSuggestions <- function(suggestdf, nsuggest) {
-    suggestdf <- ddply(suggestdf, .(suggestion), summarise, 
-                       suggestion = head(suggestion, 1),
-                       prop = max(prop))
-    suggestdf <- head(arrange(suggestdf, desc(prop)), nsuggest)
-    return(suggestdf$suggestion)
-}
+source("prediction.R")
 
 shinyServer(function(input, output) {
     
